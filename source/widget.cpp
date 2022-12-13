68489c7e8e5bebd283e6b4d891aec1044219d1a2
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-12-13 14:40:29 -0800

    Connecting to the blockchain server

--- source/widget.cpp
+++ source/widget.cpp
@@ -12,12 +12,12 @@ Widget::~Widget()
 void Widget::initialize()
 {
     tcp_blockchain_channel = new QTcpSocket();
-    tcp_blockchain_channel->connectToHost(QHostAddress("127.0.0.1"), 2100);
+    tcp_blockchain_channel->connectToHost(QHostAddress(QString("127.0.0.1")), 2100);
 
-    connect(tcp_blockchain_channel, SIGNAL(connected()), this, SLOT(new_connection()));
+    /*connect(tcp_blockchain_channel, SIGNAL(connected()), this, SLOT(new_connection()));
     connect(tcp_blockchain_channel, SIGNAL(readyRead()), this, SLOT(ready_read()));
-    connect(tcp_blockchain_channel, SIGNAL(errorOccurred(QAbstractSocket::SocketError)), this, SLOT(errorOccurred(QAbstractSocket::SocketError)));
-
+    connect(tcp_blockchain_channel, SIGNAL(errorOccurred(QAbstractSocket::SocketError )), this, SLOT(errorOccurred(QAbstractSocket::SocketError)));
+    */
 }
 
 void Widget::new_connection()
